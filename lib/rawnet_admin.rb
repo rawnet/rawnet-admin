d2833e3623da88bd4aed88ae4a5072d12b191079
Tom Beynon <tombeynon@gmail.com>
2015-01-21 16:39:41 +0000

    Added mounting helper

--- lib/rawnet_admin.rb
+++ lib/rawnet_admin.rb
@@ -2,4 +2,14 @@ require "bootstrap-sass"
 require "rawnet_admin/engine"
 
 module RawnetAdmin
+
+  def self.mount(router, &block)
+    if block_given?
+      RawnetAdmin::Engine.routes.draw do
+        instance_eval(&block)
+      end
+    end
+    router.mount RawnetAdmin::Engine => "/admin"
+  end
+
 end
