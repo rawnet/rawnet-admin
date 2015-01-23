3347299a45fe4b605c3622e55d2ef67773d78d97
Tom Beynon <tombeynon@gmail.com>
2015-01-23 10:04:34 +0000

    Removed ActiveRecord and tidied Rspec impl

--- lib/rawnet_admin/engine.rb
+++ lib/rawnet_admin/engine.rb
@@ -1,5 +1,11 @@
 module RawnetAdmin
   class Engine < ::Rails::Engine
     isolate_namespace RawnetAdmin
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.assets false
+      g.helper false
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,17 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
+require 'rspec/rails'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
